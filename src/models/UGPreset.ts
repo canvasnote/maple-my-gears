3eb9a7936aff77a44aa9f8dd384ad7c270669c4f
canvasnote <northfixiative@gmail.com>
2026-01-04 13:26:29 +0900

    stat => stats

--- src/models/UGPreset.ts
+++ src/models/UGPreset.ts
@@ -3,13 +3,13 @@ import type { IStat } from "@/gears/stat"
 export class UGPreset{
     internalName: string
     JMSName: string
-    stat: Array<IStat>
+    stats: Array<IStat>
     images: Array<string>
 
     constructor(internalName: string, JMSName: string, stat: Array<IStat>, images: Array<string>){
         this.internalName = internalName
         this.JMSName = JMSName
-        this.stat = stat
+        this.stats = stat
         this.images = images
     }
 }
